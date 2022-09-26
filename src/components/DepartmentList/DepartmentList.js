5af39e9b46612a9d8f36828223f028216b2f6bf1
Eozinum <eozinum@gmail.com>
2022-09-26 21:25:06 +0300

    add API, fetch function and edit DepartmentsList

--- src/components/DepartmentList/DepartmentList.js
+++ src/components/DepartmentList/DepartmentList.js
@@ -1,19 +1,31 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import {Box, Typography, List, ListItem, TextField, Button} from '@mui/material';
 import './style.css';
+import {getDepartments} from '../../services/fetch-data'
+import { useState } from "react";
 
 export const DepartmentList = () => {
+    const [cityName, setCityName] = useState('');
+		const [departments, setDepartments] = useState([]);
+    const onInputChange = (e) => {
+        setCityName(e.target.value);
+    }
+		
+		const onBtnClick = () => {
+			getDepartments(cityName).then((data) => setDepartments(data))
+		}
+		
+
     return (
     <Box className="departments-container">
         <Typography variant="h5">Deparments</Typography>
         <Box className="departments-form">
-            <TextField className='departments-form-input' label="Enter city" color="error" focused type='text'/>
-            <Button id='departments-form-btn' variant="contained" color="error">Get departments</Button>
+            <TextField onChange={onInputChange} className='departments-form-input' label="Enter city" color="error" focused type='text'/>
+            <Button onClick={onBtnClick} id='departments-form-btn' variant="contained" color="error">Get departments</Button>
         </Box>
         <List>
-            <ListItem>Department 1</ListItem>
-            <ListItem>Department 2</ListItem>
-            <ListItem>Department 3</ListItem>
+					{departments.map((department) => <ListItem key={department.SiteKey}>{department.Description}, {department.CityDescription}</ListItem>)}
         </List>
     </Box>
     )
