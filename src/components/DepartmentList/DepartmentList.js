89726db8baa05a62e87f4d0ce64e32c27d20a18b
Eozinum <eozinum@gmail.com>
2022-09-28 13:48:33 +0300

    fixed validation function and department-list ui

--- src/components/DepartmentList/DepartmentList.js
+++ src/components/DepartmentList/DepartmentList.js
@@ -1,32 +1,56 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import {Box, Typography, List, ListItem, TextField, Button} from '@mui/material';
-import './style.css';
-import {getDepartments} from '../../services/fetch-data'
+import {
+  Box,
+  Typography,
+  List,
+  ListItem,
+  TextField,
+  Button,
+} from "@mui/material";
+import "./style.css";
+import { getDepartments } from "../../services/fetch-data";
 import { useState } from "react";
 
 export const DepartmentList = () => {
-    const [cityName, setCityName] = useState('');
-		const [departments, setDepartments] = useState([]);
-    const onInputChange = (e) => {
-        setCityName(e.target.value);
-    }
-		
-		const onBtnClick = () => {
-			getDepartments(cityName).then((data) => setDepartments(data))
-		}
-		
+  const [cityName, setCityName] = useState("");
+  const [departments, setDepartments] = useState([]);
+  const onInputChange = (e) => {
+    setCityName(e.target.value);
+  };
 
-    return (
-    <Box className="departments-container">
-        <Typography variant="h5">Deparments</Typography>
-        <Box className="departments-form">
-            <TextField onChange={onInputChange} className='departments-form-input' label="Введіть місто" color="error" focused type='text'/>
-            <Button onClick={onBtnClick} id='departments-form-btn' variant="contained" color="error">Get departments</Button>
-        </Box>
-        <List>
-					{departments.map((department) => <ListItem key={department.SiteKey}>{department.Description}, {department.CityDescription}</ListItem>)}
-        </List>
+  const onBtnClick = () => {
+    getDepartments(cityName).then((data) => setDepartments(data));
+  };
+
+  return (
+    <Box className='departments-container'>
+      <Typography variant='h5'>Deparments</Typography>
+      <Box className='departments-form'>
+        <TextField
+          onChange={onInputChange}
+          className='departments-form-input'
+          label='Введіть місто'
+          color='error'
+          focused
+          type='text'
+        />
+        <Button
+          onClick={onBtnClick}
+          id='departments-form-btn'
+          variant='contained'
+          color='error'
+        >
+          Відобразити відділення
+        </Button>
+      </Box>
+      <List>
+        {departments.map((department, i) => (
+          <ListItem key={department.SiteKey}>
+            {i + 1}. {department.Description}, {department.CityDescription}
+          </ListItem>
+        ))}
+      </List>
     </Box>
-    )
-}
+  );
+};
--- src/services/check-input.js
+++ src/services/check-input.js
@@ -1,5 +1,11 @@
 import { TTN_LENGTH } from "../constants/constants";
 
 export const checkTtnNumber = (ttnNumber) => {
-  return !isNaN(ttnNumber) && ttnNumber.length === TTN_LENGTH ? true : false;
+  return !isNaN(ttnNumber) &&
+    ttnNumber.length === TTN_LENGTH &&
+    (ttnNumber.startsWith("1") ||
+      ttnNumber.startsWith("2") ||
+      ttnNumber.startsWith("5"))
+    ? true
+    : false;
 };
